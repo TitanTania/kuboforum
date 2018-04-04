61958696657a423546f180532a195e4e15b657d2
Kaja <kajadekker@hotmail.com>
2018-04-04 10:39:10 +0200

    Added new icon and added flex

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,5 +12,5 @@ UserManager.addUser(new User("bob","bobsen","bob420","bob@bob.bob","bobsecure","
 var user = JSON.parse(localStorage.getItem("user")); //string om til object
 
 if(UserManager.checkLogin(user)){
-	document.getElementById("login-form").innerHTML = '<form><div class="user_icon"><img src="assets/images/icon_user.png" alt=""></div><span class= username>' + user.username + '</span><button type="button" onclick="UserManager.logout()">Logout</button></form>';
+	document.getElementById("login-form").innerHTML = '<form><div class="user_icon"><img src="assets/images/icon2.png" alt=""></div><span class= username>' + user.username + '</span><button type="button" onclick="UserManager.logout()">Logout</button></form>';
 }
