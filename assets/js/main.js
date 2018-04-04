a60895e62acdbac6cd24801c4d83eee6e97b6083
Kaja <kajadekker@hotmail.com>
2018-04-04 10:08:30 +0200

    Added Javascript picture

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,3 +14,10 @@ var user = JSON.parse(localStorage.getItem("user")); //string om til object
 if(UserManager.checkLogin(user)){
 	document.getElementById("login-form").innerHTML = '<form><span class= username>' + user.username + '</span><button type="button" onclick="UserManager.logout()">Logout</button></form>';
 }
+
+var img = document.createElement("img");
+
+img.src ="assets/images/icon_user.png";
+var src = document.getElementById("login-form");
+
+src.appendChild(img);
