df0f8646106fbcc7964413affa384447797103c1
Kaja <kajadekker@hotmail.com>
2018-04-13 13:39:12 +0200

    Added some javascript to post

--- assets/js/User.js
+++ assets/js/User.js
@@ -8,3 +8,32 @@ function User(firstname, lastname, username, email, password, school){
 	this.school = school;
 }
 //this refererer til det object der bliver lavet//
+
+
+function addThread() {
+var thread = document.getElementById("thread").value;
+threads.push(thread);
+localStorage.setItem("threads", JSON.stringify(categories));
+addThreadToList(thread);
+}
+
+function addThreadToList(threadName) {
+	var liElement = document.createElement("li");
+	liElement.innerHTML=threadName;
+	document.getElementById("thread").appendChild(liElement);
+}
+
+var threads = [];
+var loadedThreads = localStorage.getItem("threads");
+if (loadedThreads != null) {
+	threads = JSON.parse(loadedThreads);
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+	for (var i = 0; i < threads.length; i++) {
+		var thread = threads[i];
+		addThreatToList(threads);
+	}
+
+	document.getElementById('id')
+})
